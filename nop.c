fe477a64786f221d054c75049763bacde17f07dc
Magdielian-code <oziomaagaecheta81@gmail.com>
2023-12-13 12:34:13 -0800

    added the nop.c file

---
+++ nop.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+
+/**
+ * _nop - nop top of stack
+ * @stack: pointer to lists for monty stack
+ * @line_number: number of line opcode occurs
+ */
+
+void _nop(__attribute__ ((unused))stack_t **stack,
+		__attribute__ ((unused)) unsigned int line_number)
+{
+	;
+}
