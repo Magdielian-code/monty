b4f1ddd6a544b403587c5a2960b64bf85dea5969
Magdielian-code <oziomaagaecheta81@gmail.com>
2023-12-13 12:39:45 -0800

    created the push.c file

---
+++ push.c
@@ -0,0 +1,27 @@
+#include "monty.h"
+
+/**
+ * _push - to push int to a stack
+ * @stack: the linked lists for monty stack
+ * @line_number: number of line opcode occurs
+ */
+ 
+void _push(stack_t **stack, __attribute__ ((unused))unsigned int line_number)
+{
+	stack_t *top;
+	(void)line_number;
+
+	top = malloc(sizeof(stack_t));
+	if (top == NULL)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
+
+	top->n = var_global.push_arg;
+	top->next = *stack;
+	top->prev = NULL;
+	if (*stack != NULL)
+		(*stack)->prev = top;
+	*stack = top;
+}
