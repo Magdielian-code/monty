be03618e9a443f45068add995f3b16e54fa9141a
Magdielian-code <oziomaagaecheta81@gmail.com>
2023-12-13 12:37:15 -0800

    created the pint.c

---
+++ pint.c
@@ -0,0 +1,20 @@
+#include "monty.h"
+
+/**
+ * _pint - print int a top of stack
+ * @stack: pointer to linked list stack
+ * @line_number: number of line opcode occurs on
+ *
+ */
+void _pint(stack_t **stack, unsigned int line_number)
+{
+	stack_t *runner;
+
+	runner = *stack;
+	if (runner == NULL)
+	{
+		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	printf("%d\n", runner->n);
+}
